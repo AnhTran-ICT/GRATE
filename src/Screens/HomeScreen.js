4dfb4f97dd51be5a3457142508f9dba18f4c5785
Vo Duc Anh (Alex) Tran <ducanhtranvo812@gmail.com>
2026-08-30 17:22:15 +1000

    SETUP initialise GRATE Expo project structure

---
+++ src/Screens/HomeScreen.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import {
+View,
+Text
+}
+from "react-native";
+
+
+export default function HomeScreen(){
+
+return(
+
+<View>
+
+<Text>
+GRATE Game Rating Platform
+</Text>
+
+</View>
+
+);
+
+}
